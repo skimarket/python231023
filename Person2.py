27ff56fd530f0d909c87ab4ee71b9ac67ac2629c
skimarket <skimarket@naver.com>
2023-10-24 14:28:35 +0900

    클래서 연습1

--- Person2.py
+++ Person2.py
@@ -1,23 +1,20 @@
-#클래스 정의
+# Person2.py 
 class Person:
-    #초기화 메서드
+    #클래스 멤버 변수
+    num_person = 0 
     def __init__(self):
-        #멤버 변수 초기화
         self.name = "default name"
-        #멤버 메서드
+        Person.num_person += 1 
     def print(self):
         print("My name is {0}".format(self.name))
 
-#인스턴스 생성
+
 p1 = Person()
 p2 = Person()
-p1.name = "전우치"
-#메서드 호출
-p1.print()
-p2.print()
-
-#런타임시에 변수 추가
+print("숫자:{0}".format(Person.num_person))
 Person.title = "new title"
 print(p1.title)
 print(p2.title)
 print(Person.title)
+
+
