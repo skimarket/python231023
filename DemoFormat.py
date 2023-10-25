cd71e5f57d9ec88ee8070143c91b713bf79ec99b
skimarket <skimarket@naver.com>
2023-10-25 10:07:10 +0900

    파일연습완료

--- DemoFormat.py
+++ DemoFormat.py
@@ -28,8 +28,23 @@ f = open("demo.txt", "wt", encoding="utf-8")
 f.write("첫번째\n두번쨰\n세번째\n")
 f.close()
 
-#파일 읽기
+#기존 파일에 첨부 a+ 기존내용유지에 추가, wt는 다 지우고 기재한 것만
 f = open("demo.txt", "wt", encoding="utf-8")
+f.write("다른 내용을 첨부\n")
+f.close()
+
+#파일 읽기
+f = open("demo.txt", "rt", encoding="utf-8")
 result = f.read()
 print(result)
+#처음으로 리셋
+f.seek(0)
+print(f.readline(), end="")
+print(f.readline(), end="")
+
+f.seek(0)
+lst = f.readlines()
+for item in lst:
+    print(item)
+
 f.close()
