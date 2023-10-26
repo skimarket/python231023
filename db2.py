5ef14d62e11a7102d0044ecdd842e637615e9780
skimarket <skimarket@naver.com>
2023-10-26 12:23:15 +0900

    클리앙중고장터검색

--- db2.py
+++ db2.py
@@ -1,8 +1,8 @@
 # db1.py 
 import sqlite3
 
-#연결객체(일단은 메모리에 저장) 
-con = sqlite3.connect(":memory:")
+#연결객체(파일에 저장)
+con = sqlite3.connect("c:\\work\\sample.db")
 #커서객체
 cur = con.cursor()
 #테이블 구조 생성
@@ -26,11 +26,8 @@ cur.executemany("insert into PhoneBook (name, phoneNum) values (?, ?);",
 
 #검색
 cur.execute("select * from PhoneBook;")
-print("---fetchone()---")
-print(cur.fetchone())
-print("---fetchmany(2)---")
-print(cur.fetchmany(2))
-print("---fetchall()---")
-cur.execute("select * from PhoneBook;")
 print(cur.fetchall())
+
+#작업 완료
+con.commit() 
 
